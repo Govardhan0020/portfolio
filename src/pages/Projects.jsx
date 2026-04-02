4b6b0ad4b17bfc6ec7220488d7e20cc45931afd8
Govardhan J <govardhanjpg6940@gmail.com>
2026-04-02 19:32:01 +0530

    loader added and resume updated

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,14 +3,20 @@ import { useTheme } from "../hooks/ThemeContext";
 
 export const Projects = () => {
   const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { theme } = useTheme();
-
 
   useEffect(() => {
     fetch("https://api.github.com/users/Govardhan0020/repos")
       .then((res) => res.json())
-      .then((data) => setRepos(data))
-      .catch((err) => console.log(err));
+      .then((data) => {
+        setRepos(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }, []);
 
   const namesToFilter = [
@@ -26,37 +32,45 @@ export const Projects = () => {
     namesToFilter.includes(item.name),
   );
 
-
   return (
-    <div className="p-4 mt-15 px-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {filteredData.map((repo) => (
-          <div
-            key={repo.id}
-            className={`p-4 rounded-lg border transition duration-300
+    <div
+      className={`p-4 mt-15 px-15
+            ${theme === "dark" ? "bg-black " : "bg-white "} `}
+    >
+      {loading ? (
+        <div className="flex justify-center items-center h-40">
+          <div className="w-10 h-10 border-8 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {filteredData.map((repo) => (
+            <div
+              key={repo.id}
+              className={`p-4 rounded-lg border transition duration-300
             ${
               theme === "dark"
                 ? "bg-black text-white border-white"
-                : "bg-white text-black border-black"
+                : "bg-white text-black border-black border-1"
             } hover:scale-105`}
-          >
-            <h2 className="text-lg font-semibold">{repo.name}</h2>
-
-            <p className="text-sm mt-1">
-              {repo.description || "No description"}
-            </p>
-
-            <a
-              href={repo.html_url}
-              target="_blank"
-              rel="noreferrer"
-              className="mt-2 inline-block underline"
             >
-              View Repo
-            </a>
-          </div>
-        ))}
-      </div>
+              <h2 className="text-lg ">{repo.name}</h2>
+
+              <p className="text-sm mt-1">
+                {repo.description || "No description"}
+              </p>
+
+              <a
+                href={repo.html_url}
+                target="_blank"
+                rel="noreferrer"
+                className="mt-2 inline-block underline"
+              >
+                View Repo
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
